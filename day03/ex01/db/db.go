989aebb4e1c2ae116b620134c531436481a1d28a
Nigelms <almazchic@gmail.com>
2023-05-24 18:23:40 +0300

    day03 ex01

--- day03/ex01/db/db.go
+++ day03/ex01/db/db.go
@@ -19,56 +19,50 @@ type Elasticsearch struct {
 	Es *elasticsearch.Client
 }
 
-func NewElasticsearch(es *elasticsearch.Client) *Elasticsearch {
-	return &Elasticsearch{es}
+func NewElasticsearch() (*Elasticsearch, error) {
+	es, err := elasticsearch.NewDefaultClient()
+	if err != nil {
+		return nil, err
+	}
+	return &Elasticsearch{es}, nil
 }
 
 func (e *Elasticsearch) GetPlaces(limit int, offset int) ([]Place, int, error) {
-	// Создаем поисковый запрос
 	query := map[string]interface{}{
 		"size": limit,
 		"from": offset,
-		// Добавьте необходимые условия фильтрации и сортировки
 	}
 
-	// Преобразуем запрос в JSON
 	queryJSON, err := json.Marshal(query)
 	if err != nil {
 		return nil, 0, err
 	}
 
-	// Создаем поисковый запрос Elasticsearch
 	req := esapi.SearchRequest{
 		Index:          []string{"places"}, // Замените на имя вашего индекса
 		Body:           strings.NewReader(string(queryJSON)),
 		TrackTotalHits: true,
 	}
 
-	// Выполняем поиск в Elasticsearch
 	res, err := req.Do(context.Background(), e.Es)
 	if err != nil {
 		return nil, 0, err
 	}
 	defer res.Body.Close()
 
-	// Проверяем статус ответа
 	if res.IsError() {
 		return nil, 0, fmt.Errorf("Elasticsearch search request failed: %s", res.String())
 	}
 
-	// Читаем ответ Elasticsearch
 	var resBody map[string]interface{}
 	if err := json.NewDecoder(res.Body).Decode(&resBody); err != nil {
 		return nil, 0, err
 	}
 
-	// Извлекаем список найденных документов (записей)
 	hits := resBody["hits"].(map[string]interface{})["hits"].([]interface{})
 
-	// Создаем слайс для хранения результатов
 	places := make([]Place, 0, len(hits))
 
-	// Итерируемся по найденным документам и преобразуем их в структуру types.Place
 	for _, hit := range hits {
 		source := hit.(map[string]interface{})["_source"]
 		placeBytes, err := json.Marshal(source)
@@ -84,9 +78,7 @@ func (e *Elasticsearch) GetPlaces(limit int, offset int) ([]Place, int, error) {
 		places = append(places, place)
 	}
 
-	// Извлекаем общее количество записей из результата поиска
 	totalHits := int(resBody["hits"].(map[string]interface{})["total"].(map[string]interface{})["value"].(float64))
 
-	// Возвращаем список записей, общее количество записей и (или) ошибку
 	return places, totalHits, nil
 }
--- day03/ex01/main.go
+++ day03/ex01/main.go
@@ -2,48 +2,77 @@ package main
 
 import (
 	"ex01/db"
-	"github.com/elastic/go-elasticsearch/v7"
 	"html/template"
 	"log"
+	"math"
 	"net/http"
+	"strconv"
 )
 
 type Store interface {
 	GetPlaces(limit int, offset int) ([]db.Place, int, error)
 }
 
-func main() {
-	es, err := elasticsearch.NewDefaultClient()
+var (
+	base Store
+	err  error
+)
+
+func init() {
+	base, err = db.NewElasticsearch()
 	if err != nil {
-		log.Fatalf("Error creating the client: %s", err)
+		log.Fatal(err)
 	}
-	db := db.NewElasticsearch(es)
-	http.HandleFunc(
-		"/", func(w http.ResponseWriter, r *http.Request) {
-			var (
-				res Data
-				err error
-			)
-			res.Places, res.Total, err = db.GetPlaces(10, 10)
-			tmpl, err := template.ParseFiles("template/index.html")
-			if err != nil {
-				http.Error(w, "Internal Server Error1", http.StatusInternalServerError)
-				return
-			}
+}
 
-			err = tmpl.Execute(w, res)
-			if err != nil {
-				log.Println(err)
-				http.Error(w, "Internal Server Error2", http.StatusInternalServerError)
-				return
-			}
-		},
-	)
-	http.ListenAndServe(":8888", nil)
-
+func main() {
+	http.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(":8888", nil))
 }
 
 type Data struct {
 	Places []db.Place
 	Total  int
+	Page   int
+	Last   int
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	var res Data
+	if res.Page, err = strconv.Atoi(r.URL.Query().Get("page")); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	limit := 10
+	offset := (res.Page - 1) * limit
+
+	res.Places, res.Total, err = base.GetPlaces(limit, offset)
+	res.Last = int(math.Ceil(float64(res.Total) / float64(limit)))
+	tmpl, err := template.New("index.html").Funcs(
+		template.FuncMap{
+			"sum": sum,
+			"sub": sub,
+		},
+	).ParseFiles("template/index.html")
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, res)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func sum(x, y int) int {
+	return x + y
+}
+
+func sub(x, y int) int {
+	return x - y
 }
