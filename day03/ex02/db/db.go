fd5ff74f07ad73d242ce5edb5378027f75e06f30
Nigelms <almazchic@gmail.com>
2023-05-25 00:10:50 +0300

    day03 ex02

---
+++ day03/ex02/db/db.go
@@ -0,0 +1,89 @@
+package db
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
+	"strings"
+)
+
+type Place struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Address  string `json:"address"`
+	Phone    string `json:"phone"`
+	Location struct {
+		Lon float64 `json:"lon"`
+		Lat float64 `json:"lat"`
+	} `json:"location"`
+}
+
+type Elasticsearch struct {
+	Es *elasticsearch.Client
+}
+
+func NewElasticsearch() (*Elasticsearch, error) {
+	es, err := elasticsearch.NewDefaultClient()
+	if err != nil {
+		return nil, err
+	}
+	return &Elasticsearch{es}, nil
+}
+
+func (e *Elasticsearch) GetPlaces(limit int, offset int) ([]Place, int, error) {
+	query := map[string]interface{}{
+		"size": limit,
+		"from": offset,
+	}
+
+	queryJSON, err := json.Marshal(query)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	req := esapi.SearchRequest{
+		Index:          []string{"places"},
+		Body:           strings.NewReader(string(queryJSON)),
+		TrackTotalHits: true,
+	}
+
+	res, err := req.Do(context.Background(), e.Es)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		return nil, 0, fmt.Errorf("Elasticsearch search request failed: %s", res.String())
+	}
+
+	var resBody map[string]interface{}
+	if err := json.NewDecoder(res.Body).Decode(&resBody); err != nil {
+		return nil, 0, err
+	}
+
+	hits := resBody["hits"].(map[string]interface{})["hits"].([]interface{})
+
+	places := make([]Place, 0, len(hits))
+
+	for _, hit := range hits {
+		source := hit.(map[string]interface{})["_source"]
+		placeBytes, err := json.Marshal(source)
+		if err != nil {
+			continue
+		}
+
+		var place Place
+		if err := json.Unmarshal(placeBytes, &place); err != nil {
+			continue
+		}
+
+		places = append(places, place)
+	}
+
+	totalHits := int(resBody["hits"].(map[string]interface{})["total"].(map[string]interface{})["value"].(float64))
+
+	return places, totalHits, nil
+}
